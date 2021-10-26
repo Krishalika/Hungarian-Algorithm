150d4c442ab01bababcd56d98526922d2c7140b8
Krishalika <krishalikadilanirathnayake@gmail.com>
2021-10-26 10:57:30 +0530

    edit - remove warning

--- hungarian_edit.py
+++ hungarian_edit.py
@@ -1,6 +1,5 @@
 import numpy as np
 import csv
-import pandas as pd
 
 from numpy.core.numeric import outer
 
@@ -140,35 +139,18 @@ def main():
 
     #The matrix who you want to find the minimum sum
     x=-1
-    # new_arr=[]
-    #with open('data1.csv', newline='') as csvfile:
-        #data = list(csv.reader(csvfile,quoting=csv.QUOTE_NONNUMERIC))
-    #cost_matrix = np.array(data)
 
     with open('data1.csv', newline='') as csvfile:
         values_in_string =[]
         task_arr = []
-        #next(csvfile)
-        #data = list(csv.reader(csvfile,quoting=csv.QUOTE_NONNUMERIC))
         data = list(csv.reader(csvfile,delimiter=','))
         for row in data[1:]:
             values_in_string.append(row[1:])
         for row in data[1:]:
             task_arr.append(row[0])
     location_arr = data[0][1:]
-    print(location_arr) # ['A', 'B']
-    print(task_arr) # ['T1', 'T2', 'T3', 'T4']
     numpy_array = np.array(values_in_string)
-    cost_matrix = numpy_array.astype(np.float)
-    # cost_matrix = np.array([[9, 11],
-	#  		    [12, 9],
-    #                          [-1,10],
-    #                          [9,2]])
-
-    # cost_matrix = np.array([[8, 11],
-	# 		    [12, 9],
-    #                         [-1,10],
-    #                         [9,20]])
+    cost_matrix = numpy_array.astype(np.float64)
     
     max_value = np.max(cost_matrix)
     #for empty value
@@ -187,7 +169,6 @@ def main():
             new_arr = np.append(cost_matrix, repeat_zero_arr, axis=0) #create new array by appending array with zeros
             ans_pos = hungarian_algorithm(new_arr.copy())#Get the element position.
             ans, ans_mat = ans_calculation(new_arr, ans_pos)#Get the minimum or maximum value and corresponding matrix.
-            #print(f"Linear Assignment problem result: {ans:.0f}\n{ans_mat}")
 
         elif len(cost_matrix) > len(cost_matrix[i]):
             zero_arr=np.repeat(0,len(cost_matrix))
@@ -195,17 +176,12 @@ def main():
             new_arr = np.hstack((cost_matrix, np.atleast_2d(repeat_zero_arr).T))
             ans_pos = hungarian_algorithm(new_arr.copy())#Get the element position.
             ans, ans_mat = ans_calculation(new_arr, ans_pos)#Get the minimum or maximum value and corresponding matrix.
-            #print(f"Linear Assignment problem result: {ans:.0f}\n{ans_mat}")
         else:
             ans_pos = hungarian_algorithm(cost_matrix.copy())#Get the element position.
             ans, ans_mat = ans_calculation(cost_matrix, ans_pos)#Get the minimum or maximum value and corresponding matrix.
             #Show the result
-           # print(f"Linear Assignment problem result: {ans:.0f}\n{ans_mat}")
-    print(f"Linear Assignment problem result: {ans:.0f}\n{ans_mat}")
     print(f"The optimal value: {ans:.0f}")
-   # output = np.nonzero(ans_mat)
     out_ind = np.transpose(np.nonzero(ans_mat))
-    print(out_ind) #[[0 0][3 1]]
 
     optimal_arr = []
     for i in out_ind:
@@ -218,8 +194,6 @@ def main():
         optimal_arr.append(sample)
     print(optimal_arr)
 
-    # final = np.array(optimal_arr)
-    # np.savetxt('output.csv', final, delimiter=',')
     with open("output.csv", "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(optimal_arr)
@@ -240,30 +214,4 @@ def main():
     print(f"Linear Assignment problem result: {ans:.0f}\n{ans_mat}")'''
 
 if __name__ == '__main__':
-    with open('data1.csv', newline='') as csvfile:
-        values_in_string =[]
-        next(csvfile)
-        #data = list(csv.reader(csvfile,quoting=csv.QUOTE_NONNUMERIC))
-        data = list(csv.reader(csvfile,delimiter=','))
-        for row in data:
-            values_in_string.append(row[1:])
-    numpy_array = np.array(values_in_string)
-    float_array = numpy_array.astype(np.float)
-    print(float_array) #[[ 9. 11.][12.  9.][-1. 10.][ 9.  2.]]
-    print(values_in_string) #[['9', '11'], ['12', '9'], ['-1', '10'], ['9', '2']]
-    
-    print(data) #[['T1', '9', '11'], ['T2', '12', '9'], ['T3', '-1', '10'], ['T4', '9', '2']]
-    # outer_out_list = []
-    # for inner_list in data:
-    #     innet_out_list = []
-    #     for string in inner_list:
-    #         innet_out_list.append(int(string))
-    #     outer_out_list.append(innet_out_list)
-    # print(outer_out_list)
-    # with open('data1.csv', 'r') as f:
-    #     next(f)
-    #     data = csv.reader(f)
-    #     data_lst = []
-    #     for line in reader:
-    #         data_lst.append([int(x) for x in line])
     main()
